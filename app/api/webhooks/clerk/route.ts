31a2594e851dd7d5ad9ab4134b832e6418af9740
stephen <osstephen70@gmail.com>
2024-06-07 03:00:24 +0100

    fixed modified webhooks route again

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,14 +1,11 @@
 /* eslint-disable camelcase */
+import { NextRequest, NextResponse } from 'next/server';
 import { WebhookEvent } from '@clerk/nextjs/server';
-import { headers } from 'next/headers';
-import { NextResponse } from 'next/server';
 import { Webhook } from 'svix';
-
 import { clerkClient } from '@clerk/clerk-sdk-node';
 import { createUser, deleteUser, updateUser } from '@/lib/actions/user.actions';
 
-export async function POST(req: Request) {
-  // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
+export async function POST(req: NextRequest) {
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
@@ -17,29 +14,23 @@ export async function POST(req: Request) {
     );
   }
 
-  // Get the headers
-  const headerPayload = headers();
-  const svix_id = headerPayload.get('svix-id');
-  const svix_timestamp = headerPayload.get('svix-timestamp');
-  const svix_signature = headerPayload.get('svix-signature');
+  const svix_id = req.headers.get('svix-id');
+  const svix_timestamp = req.headers.get('svix-timestamp');
+  const svix_signature = req.headers.get('svix-signature');
 
-  // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response('Error occured -- no svix headers', {
+    return new NextResponse('Error occurred -- no svix headers', {
       status: 400,
     });
   }
 
-  // Get the body
   const payload = await req.json();
   const body = JSON.stringify(payload);
 
-  // Create a new Svix instance with your secret.
   const wh = new Webhook(WEBHOOK_SECRET);
 
   let evt: WebhookEvent;
 
-  // Verify the payload with the headers
   try {
     evt = wh.verify(body, {
       'svix-id': svix_id,
@@ -48,18 +39,23 @@ export async function POST(req: Request) {
     }) as WebhookEvent;
   } catch (err) {
     console.error('Error verifying webhook:', err);
-    return new Response('Error occured', {
+    return new NextResponse('Error occurred', {
       status: 400,
     });
   }
 
-  // Get the ID and type
   const { id } = evt.data;
   const eventType = evt.type;
 
-  // CREATE
+  if (id === undefined) {
+    console.error('Webhook event data does not contain an ID');
+    return new NextResponse('Error occurred -- no ID in webhook event data', {
+      status: 400,
+    });
+  }
+
   if (eventType === 'user.created') {
-    const { id, email_addresses, image_url, first_name, last_name, username } =
+    const { email_addresses, image_url, first_name, last_name, username } =
       evt.data;
 
     const user = {
@@ -73,7 +69,6 @@ export async function POST(req: Request) {
 
     const newUser = await createUser(user);
 
-    // Set public metadata
     if (newUser) {
       await clerkClient.users.updateUser(id, {
         publicMetadata: {
@@ -85,9 +80,8 @@ export async function POST(req: Request) {
     return NextResponse.json({ message: 'OK', user: newUser });
   }
 
-  // UPDATE
   if (eventType === 'user.updated') {
-    const { id, image_url, first_name, last_name, username } = evt.data;
+    const { image_url, first_name, last_name, username } = evt.data;
 
     const user = {
       firstName: first_name || '',
@@ -101,17 +95,14 @@ export async function POST(req: Request) {
     return NextResponse.json({ message: 'OK', user: updatedUser });
   }
 
-  // DELETE
   if (eventType === 'user.deleted') {
-    const { id } = evt.data;
-
-    const deletedUser = await deleteUser(id!);
+    const deletedUser = await deleteUser(id);
 
     return NextResponse.json({ message: 'OK', user: deletedUser });
   }
 
-  console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
+  console.log(`Webhook with an ID of ${id} and type of ${eventType}`);
   console.log('Webhook body:', body);
 
-  return new Response('', { status: 200 });
+  return new NextResponse('', { status: 200 });
 }
